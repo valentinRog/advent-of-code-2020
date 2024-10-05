498e05790715a5a8253a50681f52d3c71e692fb2
Valentin Rogister <valentin.rogister@gmail.com>
2024-10-05 16:16:41 +0200

    day15

---
+++ day15/part1.rkt
@@ -0,0 +1,12 @@
+#lang racket
+
+(define data
+  (map string->number
+       (string-split (string-trim (port->string (current-input-port))) ",")))
+
+(define m (for/hash ((n data) (i (in-range (- (length data) 1)))) (values n i)))
+
+(let loop ((prev (last data)) (m m) (i (- (length data) 1)))
+  (cond ((= i (- 2020 1)) prev)
+        ((hash-has-key? m prev) (loop (- i (hash-ref m prev)) (hash-set m prev i) (add1 i)))
+        (#t (loop 0 (hash-set m prev i) (add1 i)))))
