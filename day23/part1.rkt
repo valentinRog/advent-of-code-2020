006f4d19f640a5e3b2e4212020f6c1a7d9c7bc88
Valentin Rogister <valentin.rogister@gmail.com>
2024-10-11 13:26:59 +0200

    better day23 part1

--- day23/part1.rkt
+++ day23/part1.rkt
@@ -1,31 +1,74 @@
 #lang racket
 
+(struct s-node (v left right))
+
 (define data
   (map string->number (map string (string->list (string-trim (port->string (current-input-port)))))))
 
-(define (rotate l) (append (cdr l) (list (car l))))
+(define v (list->vector data))
 
-(define (rotate-to l c) (if (= (car l) c) l (rotate-to (rotate l) c)))
+(define m (for/hash ((n data)) (values n (box (s-node n '() '())))))
 
-(define (list-max l)
-  (for/fold ((acc #f)) ((e l))
-    (match acc
-      (#f e)
-      (acc #:when (< acc e) e)
-      (_ acc))))
+(define four-max (take (sort data >) 4))
 
-(define (insert-second l ll) (cons (car l) (append ll (cdr l))))
+(define (left node) (s-node-left (unbox node)))
+(define (right node) (s-node-right (unbox node)))
+(define (val node) (s-node-v (unbox node)))
 
-(define (move l)
-  (define (find-target l c i)
-    (match (cons (member (- c i) l) (- c i))
-      ((cons #f 0) (list-max l))
-      ((cons #f _) (find-target l c (add1 i)))
-      ((cons _ _)  (- c i))))
-  (let* ((tmp (take (cdr l) 3)) (c (car l)) (l (cons c (drop (cdr l) 3))))
-    (rotate (rotate-to (insert-second (rotate-to l (find-target l c 1)) tmp) c))))
+(for ((i (in-range (vector-length v))))
+  (let ((node (hash-ref m (vector-ref v i)))
+        (left (hash-ref m (vector-ref v (modulo (- i 1) (vector-length v)))))
+        (right (hash-ref m (vector-ref v (modulo (add1 i) (vector-length v))))))
+    (set-box! node (s-node (val node) left right))))
 
-(displayln
- (string-join (map number->string
-                   (drop (rotate-to (for/fold ((l data)) ((_ (in-range 100))) (move l)) 1) 1))
-              ""))
+(define (drop-3-right node)
+  (let ((new-right-node (right (right (right (right node))))))
+    (set-box! node (s-node (val node)
+                           (left node)
+                           new-right-node))
+    (set-box! new-right-node (s-node (val new-right-node)
+                                     node
+                                     (right new-right-node)))))
+
+(define (insert-3-right node inserting-node)
+  (let ((right-to-inserting (right node))
+        (last-inserting (right (right inserting-node))))
+    (set-box! node (s-node
+                    (val node)
+                    (left node)
+                    inserting-node))
+    (set-box! right-to-inserting (s-node
+                                  (val right-to-inserting)
+                                  last-inserting
+                                  (right right-to-inserting)))
+    (set-box! inserting-node (s-node
+                              (val inserting-node)
+                              node
+                              (right inserting-node)))
+    (set-box! last-inserting (s-node
+                              (val last-inserting)
+                              (left last-inserting)
+                              right-to-inserting))))
+
+(define (make-res)
+  (car
+   (for/fold ((acc (cons "" (right (hash-ref m 1))))) ((_ (in-range (- (length data) 1))))
+     (let ((s (car acc)) (node (cdr acc)))
+       (cons (string-append s (number->string (val node))) (right node))))))
+
+(define (compute head k)
+  (define (find-target c hs)
+    (let loop ((i 1))
+      (cond
+        ((= (- c i) 0) (for/first ((n four-max) #:when (not (set-member? hs n))) n))
+        ((set-member? hs (- c i)) (loop (add1 i)))
+        (#t (- c i)))))
+  (if (= k 0)
+      (make-res)
+      (let* ((tmp (right head))
+             (destination (hash-ref m (find-target (val head) (set (val tmp) (val (right tmp)) (val (right (right tmp))))))))
+        (drop-3-right head)
+        (insert-3-right destination tmp)
+        (compute (right head) (- k 1)))))
+
+(displayln (compute (hash-ref m (car data)) 100))
